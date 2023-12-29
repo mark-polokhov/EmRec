d5113cd979582e9af9db6d63f6130d317ffb0575
Mark Polokhov <myupolokhov@edu.hse.ru>
2023-12-29 17:26:08 +0300

    Torch DataLoader added

--- train.py
+++ train.py
@@ -1,7 +1,8 @@
+from dataset import MultiDataset, apply_transform
+
 import config
 import argparse
-
-from dataset import MultiDataset, apply_transform
+from torch.utils.data import DataLoader
 
 
 parser = argparse.ArgumentParser(prog='EmRec')
@@ -10,18 +11,24 @@ parser.add_argument('-a', dest='all_datasets', action='store_true',
                     help='Use all available datasets')
 parser.add_argument('-d', dest='datasets', type=str, nargs='+',
                     help='Datasets to use')
-parser.add_argument('--img_size', type=int, default=128,
+parser.add_argument('--img_size', type=int,
                     help='Every image in the Dataset will be transform to img_size')
 parser.add_argument('--transform', type=str, default='default',
                     help='What tranform use for images in Dataset')
+# Other
+parser.add_argument('-b', '--batch_size', type=int,
+                    help='Batch size for training')
+parser.add_argument('-j', '--num_workers', type=int, default=4,
+                    help='Number of workers')
 
 args = parser.parse_args()
 
 
-def init_training():
+def run_training():
     dataset = MultiDataset(args, transform=apply_transform(args))
-    print(dataset[0])
+    dataloader = DataLoader(dataset, batch_size=args.batch_size, shuffle=True,
+                            num_workers=args.num_workers)
 
 
 if __name__ == '__main__':
-    init_training()
+    run_training()
